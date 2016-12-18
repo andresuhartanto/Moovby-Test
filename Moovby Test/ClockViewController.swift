50929f74be58f7c8d2c9f89eb54943f9b0501024
andresuhartanto <andresuhartanto28@gmail.com>
2016-12-18 14:29:57 +0800

    first commit

---
+++ Moovby Test/ClockViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Moovby Test
+//
+//  Created by Andre Suhartanto on 12/18/16.
+//  Copyright © 2016 EndeJeje. All rights reserved.
+//
+
+import UIKit
+
+class ClockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
