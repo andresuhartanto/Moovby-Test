ff91c375f1cf11d7db79f749d599846f5f7bd99b
andresuhartanto <andresuhartanto28@gmail.com>
2016-12-19 11:51:59 +0800

    second commit

--- Moovby Test/ClockViewController.swift
+++ Moovby Test/ClockViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ClockViewController: UIViewController {
-
+    
+    var countdown = 600
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadTime()
+        Timer.scheduledTimer(timeInterval: 600, target: self, selector: #selector(ClockViewController.loadTime), userInfo: nil, repeats: true)
+        
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ClockViewController.countdownFunc), userInfo: nil, repeats: true)
     }
+    
+    func loadTime(){
+        Alamofire.request("http://api.timezonedb.com/v2/get-time-zone?key=5HHSK5B7NCLT&format=json&by=position&lat=3.1385036&lng=101.6169489").responseJSON(completionHandler: { (JSONResponse) in
+            switch JSONResponse.result{
+            case .success(let responseValuse):
+                let jsonValue = JSON(responseValuse)
+                
+                guard let timeURL = jsonValue["formatted"].string else { return }
+                    self.timeLabel.text = timeURL
+                
+            case .failure(let error):
+                print(error.localizedDescription)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            }
+        }
+        )
     }
-
-
+    
+    func countdownFunc(){
+        if self.countdown > 0{
+            self.timerLabel.text = "\(String(self.countdown)) seconds to referesh"
+            self.countdown -= 1
+        }else{
+            self.countdown = 600
+        }
+    }
+    
+    @IBAction func onRefreshButtonPressed(_ sender: UIButton) {
+        loadTime()
+        self.countdown = 600
+    }
+    
+    
+    
 }
 
